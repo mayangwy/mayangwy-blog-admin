a2112811a5ee7104703de7a31c286d42d5732b30
mayang@winning.com.cn <mayang@winning.com.cn>
2019-11-12 17:18:10 +0800

    添加hutools依赖

--- mayangwy-blog-admin-common/src/main/java/org/mayangwy/blog/admin/common/jdbc/QueryRunnerBuilder.java
+++ mayangwy-blog-admin-common/src/main/java/org/mayangwy/blog/admin/common/jdbc/QueryRunnerBuilder.java
@@ -2,8 +2,6 @@ package org.mayangwy.blog.admin.common.jdbc;
 
 import org.apache.commons.dbutils.QueryRunner;
 
-import javax.sql.DataSource;
-
 public class QueryRunnerBuilder {
 
     private static QueryRunner queryRunner = new QueryRunner(DruidDataSourceBuilder.getDataSource());
